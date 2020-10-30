91bda179ab880405e7f730cc2cd1b1a00a9d4812
Joshua Jensch <jenschjoshua@gmail.com>
2020-10-30 18:31:58 +0100

    shift initialization of SubsonicContextProvider into the class, instead of main file and use provider for db hookup

--- lib/provider/subsonic_context_provider.dart
+++ lib/provider/subsonic_context_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:sonicear/db/dao/sqflite_server_dao.dart';
 import 'package:sonicear/subsonic/context.dart';
 
 class SubsonicContextProvider with ChangeNotifier {
@@ -8,5 +9,21 @@ class SubsonicContextProvider with ChangeNotifier {
   void updateContext(SubsonicContext context) {
     _context = context;
     notifyListeners();
+  }
+
+  Future<void> initialize(SqfliteServerDao dao) async {
+    final active = await dao.getActiveServer();
+
+    if(active != null) {
+      updateContext(active);
+      return;
+    }
+
+    final servers = await dao.listServers();
+    if(servers.isEmpty)
+      return;
+
+    await dao.setActiveServer(servers.first);
+    updateContext(servers.first);
   }
 }
